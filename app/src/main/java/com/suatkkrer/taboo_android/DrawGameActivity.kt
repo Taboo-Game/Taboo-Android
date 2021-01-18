ffcf1b137a954d24ec1e3bae8ee9ce3bba4d707f
suatkkrer <suat.kukrer@gmail.com>
2021-01-18 12:04:59 +0100

    settings activity

---
+++ app/src/main/java/com/suatkkrer/taboo_android/DrawGameActivity.kt
@@ -0,0 +1,11 @@
+package com.suatkkrer.taboo_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrawGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draw_game)
+    }
+}
