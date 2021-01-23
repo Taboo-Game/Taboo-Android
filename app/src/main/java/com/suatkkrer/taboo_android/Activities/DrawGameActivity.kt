3aff22caa18505bb9a03b492f28ab35ae3c70295
suatkkrer <suat.kukrer@gmail.com>
2021-01-23 15:54:28 +0100

    canvas added

--- app/src/main/java/com/suatkkrer/taboo_android/Activities/DrawGameActivity.kt
+++ app/src/main/java/com/suatkkrer/taboo_android/Activities/DrawGameActivity.kt
@@ -1,15 +1,25 @@
 package com.suatkkrer.taboo_android.Activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.suatkkrer.taboo_android.R
 
 class DrawGameActivity : AppCompatActivity() {
+
+    private var canvasView: CanvasView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_draw_game)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
+
+        canvasView = findViewById(R.id.canvas) as CanvasView
+
+    }
+
+    fun clearCanvas(view: View) {
+        canvasView!!.clearCanvas()
     }
 }
